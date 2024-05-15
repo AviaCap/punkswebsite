db147b4903ea4385b218f5d6c4fcde1d4b1a0b6b
Musya <musya.ilyasova@getorca.app>
2024-05-15 18:00:26 +0300

    fixed menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,12 @@ import themeSwitche from "./modules/themeSwitcher";
 import toggleBurger, {scrollPage, toggleMenu} from "./modules/menu"
 
 document.addEventListener("DOMContentLoaded", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  window.addEventListener('resize', () => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
   themeSwitche();
   toggleBurger();
   scrollPage();
